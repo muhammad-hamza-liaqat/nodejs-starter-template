20133a965f7bf28564c6bc4f6a84857c0e459000
Muhammad-Hamza-Liaqat <mh408800@gmail.com>
2025-07-08 06:48:24 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+
+const express = require("express");
+const path = require("path")
+
+const cors = require('./config/corsConfig')
+
+
+const app = express();
+
+app.use(cors)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+    res.render('startServer.ejs', { port: PORT });
+});
+
+app.listen(PORT, () => {
+    console.warn(`🚀 Node.js server is running at http://localhost:${PORT}/`)
+})
